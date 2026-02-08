062371faa2bedac69c767fcff8af56388109c791
xdecker <xavier_deckza@hotmail.com>
2026-02-08 00:01:24 -0500

    feat(pokeapi): config lib client

--- lib/pokeapi.ts
+++ lib/pokeapi.ts
@@ -1,16 +1,25 @@
+import { PokemonsResponse } from "@/interfaces/pokemonsResponse";
+import { SimplePokemon } from "@/interfaces/simple-pokemon";
+
 const POKEAPI_URL = process.env.POKEAPI_URL;
 
-export async function getAllPokemons(limit:number = 20, offset:number = 0){
+export async function getAllPokemons(limit: number = 20, offset: number = 0) {
+  const response: PokemonsResponse = await fetch(
+    `${POKEAPI_URL}?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
 
+  //simplify information to list
+  const pokemons: SimplePokemon[] = response.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!, //https://pokeapi.co/api/v2/pokemon/1/"
+    name: pokemon.name,
+  }));
+  return pokemons;
 }
 
 export async function searchPokemon(name: string) {
-    const res = await fetch(
-      `${POKEAPI_URL}/${name.toLowerCase()}`
-    );
-  
-    if (!res.ok) throw new Error('Pokemon not found');
-  
-    return res.json();
-  }
-  
+  const res = await fetch(`${POKEAPI_URL}/${name.toLowerCase()}`);
+
+  if (!res.ok) throw new Error("Pokemon not found");
+
+  return res.json();
+}
