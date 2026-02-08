da35c0908fdbec34abdeeb995eaa7b5a90f97d72
xdecker <xavier_deckza@hotmail.com>
2026-02-08 18:45:29 -0500

    feat: add method to get pokemons of favorite code

--- lib/pokefavsapi.ts
+++ lib/pokefavsapi.ts
@@ -1,20 +1,32 @@
+import { FavoriteConsultResponse } from "@/interfaces/favoriteConsultResponse";
+import { SimplePokemon } from "@/interfaces/simple-pokemon";
 
 const POKEFAVAPI_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const onSaveFavorites = async (favorites:string[]) => {
-    const res = await fetch(
-      `${POKEFAVAPI_URL}/favorites`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          pokemons: favorites,
-        }),
-      }
-    );
+export const onSaveFavorites = async (favorites: SimplePokemon[]) => {
+  const res = await fetch(`${POKEFAVAPI_URL}/favorites`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      pokemons: favorites,
+    }),
+  });
 
-    const data = await res.json();
-    return data;
-  };
+  const data = await res.json();
+  return data;
+};
+
+export const getFavoritesByCode = async (code: string) => {
+  if (code.length > 0) {
+    const res:FavoriteConsultResponse = await fetch(`${POKEFAVAPI_URL}/favorites/${code}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => res.json());
+    
+    return res;
+  }
+};
