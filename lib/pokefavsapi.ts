a53cb75a5500530ee083a3843e2dda11a29d5464
xdecker <xavier_deckza@hotmail.com>
2026-02-08 16:52:44 -0500

    feat: save list method

---
+++ lib/pokefavsapi.ts
@@ -0,0 +1,20 @@
+
+const POKEFAVAPI_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+export const onSaveFavorites = async (favorites:string[]) => {
+    const res = await fetch(
+      `${process.env.POKEFAVAPI_URL}/favorites`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          pokemons: favorites,
+        }),
+      }
+    );
+
+    const data = await res.json();
+    return data;
+  };
